38b92d25a7f8d37652c2da931f9c9e2fbcf4569b
Jesús Mejía <jesusmejded@gmail.com>
2020-05-10 18:11:03 -0500

    update aside and insert category link

--- single.php
+++ single.php
@@ -7,29 +7,48 @@
 <?php
 	while (have_posts()) :	the_post();
   $super_id = get_the_ID();
+	$term = get_queried_object();
 ?>
 
-<div class="row">
+<div class="row mt-5">
 
 
 
     <div class="blog-post col-sm-8">
-            <h2 class="blog-post-title"><?php  the_title(); ?></h2>
-            <p class="blog-post-meta"><?php echo get_the_date(); ?> by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>"><?php the_author(); ?></a></p>
+            <h1 class="blog-post-title"><?php  the_title(); ?></h1>
+            <p class="blog-post-meta">
+							<?php echo get_the_date(); ?>
+							by
+							<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>">
+								<?php the_author(); ?>
+							</a>
+						<!-- <div class="bs-component"> -->
+							<?php
+							//get all the categories the post belongs to
+							$categories = wp_get_post_categories( get_the_ID() );
+							//loop through them
+							foreach($categories as $c){
+								$cat = get_category( $c );
+								//get the name of the category
+								$cat_id = get_cat_ID( $cat->name );
+								//make a list item containing a link to the category
+								echo '<a href="'.get_category_link($cat_id).'" class="badge badge-dark ml-2" >'.$cat->name.'</a>';
+							}
+							?>
+						</p>
+						<!-- </div> -->
 
             <?php the_content(); ?>
 
-          <!-- <nav class="blog-pagination">
-            <a class="btn btn-outline-primary" href="#">Older</a>
-            <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
-          </nav> -->
+
+					<!-- <h3>Category</h3> -->
 
           <?php
              // the query
-             $term = get_queried_object();
+
              $the_query = new WP_Query( array(
                'category_name' => $term->slug,
-                'posts_per_page' => 3,
+                'posts_per_page' => 4,
              ));
           ?>
 
@@ -81,9 +100,45 @@
 ?>
   <!-- aside -->
   <div class="col-sm-4">
+		<?php
+		$the_query = new WP_Query( array(
+			'category_name' => $term->slug,
+			 'posts_per_page' => 3,
+		));
+		// start loop
+		while ($the_query->have_posts()) : $the_query->the_post();
+			if (get_the_ID() == $super_id) {
+				continue;
+			}
+		?>
+		<div class="card mb-3">
+			<div style="width: 100%;height: 100%;min-height: 200px;background: url(<?php echo get_the_post_thumbnail_url(get_the_ID(),'cool_blog-featured-image'); ?>) no-repeat center; background-size: 100% auto;">
+			</div>
+			<!-- <svg class="bd-placeholder-img card-img-bottom" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg> -->
+		  <!-- <img src="..." class="card-img-top" alt="..."> -->
+		  <div class="card-body">
+		    <h5 class="card-title"><?php the_title(); ?></h5>
+		    <a href="<?php echo get_permalink(); ?>" class="stretched-link">Read</a>
+		  </div>
+		</div>
+		<?php
+		// end loop
+		endwhile;
+		?>
+
+
+
+		<!-- =============================== -->
+		<!-- =============================== -->
+		<!-- =============================== -->
+		<!-- =============================== -->
     <?php if ( is_active_sidebar( 'sidebar-posts' ) ) : ?>
       <?php dynamic_sidebar( 'sidebar-posts' ); ?>
     <?php endif; ?>
+
+
+
+
   </div>
 </div>
 </div>
